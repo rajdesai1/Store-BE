78fb05e14643c0f11ca3978f7942c5234e36523f
= <=>
2023-03-01 17:16:22 +0530

    settings.py allowed host

--- Darwin/Darwin/settings.py
+++ Darwin/Darwin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-003oo_(urn&%(5*-1i**_mlipq5se_p7uy7x7$8uaxapz1*s#o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 ]
 
 #cors config
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:3000',
 )
